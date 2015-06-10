af8e58d2daac26bf51f0d11234ec6646a7590138
jarstingstall <jrarst01@gmail.com>
2015-06-10 10:57:07 -0400

    add new progression button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import {ProgressionGenerator} from './ProgressionGenerator';
 import './styles/app.scss';
 
-var progression = ProgressionGenerator.generate(
-    4, 
-    ['maj7', 'm7', 'maj7#11', '7sus4', 'm7b13', 'm7b5']
-);
+function generateNewProgression() {
+    var progression = ProgressionGenerator.generate(
+        4, 
+        ['maj7', 'm7', 'maj7#11', '7sus4', 'm7b13', 'm7b5']
+    );
 
-document.querySelector('.progression').innerHTML = buildHTML();
+    document.querySelector('.progression').innerHTML = buildHTML();
 
-function buildHTML() {
-    var html = '';
-    progression.forEach(chord => {
-        html += `<div class="chord" data-chord-type="${chord.type}"><p>${chord.symbol}</p></div>`;
-    });
-    return html;
+    function buildHTML() {
+        var html = '';
+        progression.forEach(chord => {
+            html += `<div class="chord" data-chord-type="${chord.type}"><p>${chord.symbol}</p></div>`;
+        });
+        return html;
+    }
 }
+
+document.querySelector('.new-progression').addEventListener('click', generateNewProgression);
+
+generateNewProgression();
