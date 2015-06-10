41636e596de5c245024475044a2bf96cd8bc4125
jarstingstall <jrarst01@gmail.com>
2015-06-10 09:06:15 -0400

    add colors and generate markup in js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import './styles/app.scss';
 
 var progression = ProgressionGenerator.generate(
     4, 
-    ['maj7', 'm7', 'maj7#11', '7sus4', 'm7b5']
+    ['maj7', 'm7', 'maj7#11', '7sus4', 'm7b13', 'm7b5']
 );
 
-console.log(progression);
+document.querySelector('.progression').innerHTML = buildHTML();
+
+function buildHTML() {
+    var html = '';
+    progression.forEach(chord => {
+        html += `<div class="chord" data-chord-type="${chord.type}"><p>${chord.symbol}</p></div>`;
+    });
+    return html;
+}
