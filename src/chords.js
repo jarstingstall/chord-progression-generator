e6c0f05ad261c46b84bdcdae4aa815a00b54dba4
jarstingstall <jrarst01@gmail.com>
2015-06-09 20:41:22 -0400

    load all chords from diatonic major scale

---
+++ src/chords.js
@@ -0,0 +1,10 @@
+export var chords = {
+
+    maj7: ['Cmaj7', 'Dbmaj7', 'Dmaj7', 'Ebmaj7', 'Emaj7', 'Fmaj7', 'Gbmaj7', 'Gmaj7', 'Abmaj7', 'Amaj7', 'Bbmaj7', 'Bmaj7'],
+    m7: ['Cm7', 'C#m7', 'Dm7', 'Ebm7', 'Em7', 'Fm7', 'F#m7', 'Gm7', 'Abm7', 'Am7', 'Bbm7', 'Bm7'],
+    "maj7#11": ['Cmaj7#11', 'Dbmaj7#11', 'Dmaj7#11', 'Ebmaj7#11', 'Emaj7#11', 'Fmaj7#11', 'Gbmaj7#11', 'Gmaj7#11', 'Abmaj7#11', 'Amaj7#11', 'Bbmaj7#11', 'Bmaj7#11'],
+    "7sus4": ['C7sus4', 'Db7sus4', 'D7sus4', 'Eb7sus4', 'E7sus4', 'F7sus4', 'F#7sus4', 'G7sus4', 'Ab7sus4', 'A7sus4', 'Bb7sus4', 'B7sus4'],
+    m7b6: ['Cm7b6', 'C#m7b6', 'Dm7b6', 'D#m7b6', 'Em7b6', 'Fm7b6', 'F#m7b6', 'Gm7b6', 'G#m7b6', 'Am7b6', 'Bbm7b6', 'Bm7b6'],
+    m7b5: ['Cm7b5', 'C#m7b5', 'Dm7b5', 'D#m7b5', 'Em7b5', 'Fm7b5', 'F#m7b5', 'Gm7b5', 'G#m7b5', 'Am7b5', 'Bbm7b5', 'Bm7b5']
+
+};
