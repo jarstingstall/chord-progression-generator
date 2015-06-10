e521b15f438a2d9ebf6796addb9667992843c508
jarstingstall <jrarst01@gmail.com>
2015-06-09 23:58:25 -0400

    setup bourbon, bitters, and neat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import {ProgressionGenerator} from './ProgressionGenerator';
+import './styles/app.scss';
 
-var progression = ProgressionGenerator.generate(4, ['m7', '7sus4', 'maj7#11']);
+var progression = ProgressionGenerator.generate(
+    4, 
+    ['maj7', 'm7', 'maj7#11', '7sus4', 'm7b5']
+);
+
+console.log(progression);
