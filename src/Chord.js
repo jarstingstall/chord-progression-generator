cd8f3a8ac0736db2311c82c2c3fc929d8b9499d0
jarstingstall <jrarst01@gmail.com>
2015-06-16 22:21:20 -0400

    remove unused methods

--- src/Chord.js
+++ src/Chord.js
@@ -5,14 +5,4 @@ export class Chord {
         this.root = options.root;
         this.subs = options.subs;
     }
-
-    get relativeMode() {
-        return `${this._getOrdinal(this.mode.order)} mode of ${this.parentKey} ${this.parentTonality}`
-    }
-
-    _getOrdinal(n) {
-        var s=["th","st","nd","rd"],
-        v=n%100;
-        return n+(s[(v-20)%10]||s[v]||s[0]);
-    }
 }
